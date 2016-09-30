ce15bdd76cb4b82ace258e542eac507ababc3e41
soujiro27 <jc.raus27@gmail.com>
2016-09-30 13:26:34 -0500

    con estilos ya terminados

---
+++ cargaAutor.php
@@ -0,0 +1,14 @@
+<?php
+include 'conecta.php';
+
+$query="select * from autor";
+$resultado=mysql_query($query);
+$autores=array();
+$macedo=0;
+while($campos=mysql_fetch_array($resultado))
+{
+  $autores[$macedo]=array('id'=>$campos['id'],'nombre'=>$campos['nombre']);
+  $macedo=$macedo+1;
+}
+echo json_encode($autores);
+?>
